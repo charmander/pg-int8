91d0683b3d0f9b24176bbbffbd03680cd4c97b17
Charmander <~@charmander.me>
2017-11-14 14:23:15 -0800

    Make compatible with Node 0.12
    (No performance change on Node 9.1.0.)

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 
 // selected so (BASE - 1) * 0x100000000 + 0xffffffff is a safe integer
-const BASE = 1000000;
+var BASE = 1000000;
 
-const readInt8 = buffer => {
-	let high = buffer.readInt32BE(0);
-	let low = buffer.readUInt32BE(4);
-	let sign = '';
+function readInt8(buffer) {
+	var high = buffer.readInt32BE(0);
+	var low = buffer.readUInt32BE(4);
+	var sign = '';
 
 	if (high < 0) {
 		high = ~high + (low === 0);
@@ -14,24 +14,30 @@ const readInt8 = buffer => {
 		sign = '-';
 	}
 
-	let result = '';
+	var result = '';
+	var carry;
+	var t;
+	var digits;
+	var pad;
+	var l;
+	var i;
 
 	{
-		const carry = high % BASE;
+		carry = high % BASE;
 		high = high / BASE >>> 0;
 
-		const t = 0x100000000 * carry + low;
+		t = 0x100000000 * carry + low;
 		low = t / BASE >>> 0;
-		const digits = '' + (t - BASE * low);
+		digits = '' + (t - BASE * low);
 
 		if (low === 0 && high === 0) {
 			return sign + digits + result;
 		}
 
-		let pad = '';
-		const l = 6 - digits.length;
+		pad = '';
+		l = 6 - digits.length;
 
-		for (let i = 0; i < l; i++) {
+		for (i = 0; i < l; i++) {
 			pad += '0';
 		}
 
@@ -39,21 +45,21 @@ const readInt8 = buffer => {
 	}
 
 	{
-		const carry = high % BASE;
+		carry = high % BASE;
 		high = high / BASE >>> 0;
 
-		const t = 0x100000000 * carry + low;
+		t = 0x100000000 * carry + low;
 		low = t / BASE >>> 0;
-		const digits = '' + (t - BASE * low);
+		digits = '' + (t - BASE * low);
 
 		if (low === 0 && high === 0) {
 			return sign + digits + result;
 		}
 
-		let pad = '';
-		const l = 6 - digits.length;
+		pad = '';
+		l = 6 - digits.length;
 
-		for (let i = 0; i < l; i++) {
+		for (i = 0; i < l; i++) {
 			pad += '0';
 		}
 
@@ -61,21 +67,21 @@ const readInt8 = buffer => {
 	}
 
 	{
-		const carry = high % BASE;
+		carry = high % BASE;
 		high = high / BASE >>> 0;
 
-		const t = 0x100000000 * carry + low;
+		t = 0x100000000 * carry + low;
 		low = t / BASE >>> 0;
-		const digits = '' + (t - BASE * low);
+		digits = '' + (t - BASE * low);
 
 		if (low === 0 && high === 0) {
 			return sign + digits + result;
 		}
 
-		let pad = '';
-		const l = 6 - digits.length;
+		pad = '';
+		l = 6 - digits.length;
 
-		for (let i = 0; i < l; i++) {
+		for (i = 0; i < l; i++) {
 			pad += '0';
 		}
 
@@ -83,12 +89,12 @@ const readInt8 = buffer => {
 	}
 
 	{
-		const carry = high % BASE;
-		const t = 0x100000000 * carry + low;
-		const digits = '' + t % BASE;
+		carry = high % BASE;
+		t = 0x100000000 * carry + low;
+		digits = '' + t % BASE;
 
 		return sign + digits + result;
 	}
-};
+}
 
 module.exports = readInt8;
