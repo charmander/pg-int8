cbf7d814d83117a9a10c71268db74638ed43d626
Charmander <~@charmander.me>
2018-01-11 04:28:55 -0800

    Correct WebAssembly implementation and optimize its zero-padding

--- benchmarks/webassembly-single-return/index.js
+++ benchmarks/webassembly-single-return/index.js
@@ -7,22 +7,22 @@ const binary = fs.readFileSync(path.join(__dirname, 'parseint8.wasm'));
 const {parseLow, parseHigh} = new WebAssembly.Instance(new WebAssembly.Module(binary)).exports;
 
 module.exports = buffer => {
-	const high = buffer.readInt32BE(0);
-	const low = buffer.readUInt32BE(4);
+	let high = buffer.readInt32BE(0);
+	let low = buffer.readUInt32BE(4);
+	let sign = '';
 
-	if (high === 0 || high === -1) {
-		return '' + (low | 0);
+	if (high < 0) {
+		high = ~high + (low === 0);
+		low = (~low + 1) >>> 0;
+		sign = '-';
 	}
 
-	const lowDecimal = '' + parseLow(high, low);
+	if (high === 0) {
+		return sign + low;
+	}
+
 	const highDecimal = parseHigh(high, low);
+	const lowDecimal = ('' + (1000000000 + parseLow(high, low))).slice(1);
 
-	let pad = '';
-	const l = 9 - lowDecimal.length;
-
-	for (let i = 0; i < l; i++) {
-		pad += '0';
-	}
-
-	return '' + highDecimal + pad + lowDecimal;
+	return sign + highDecimal + lowDecimal;
 };
--- benchmarks/webassembly-single-return/parseint8.wat
+++ benchmarks/webassembly-single-return/parseint8.wat
@@ -8,7 +8,7 @@
 
 (func (export "parseHigh") (param $high i32) (param $low i32) (result f64)
 	(f64.convert_s/i64
-		(i64.div_s
+		(i64.div_u
 			(i64.or
 				(i64.shl (i64.extend_u/i32 (get_local $high)) (i64.const 32))
 				(i64.extend_u/i32 (get_local $low)))
