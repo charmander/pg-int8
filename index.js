7f3aa2dacb80b635577d830b3c9bf02abf3b2849
Charmander <~@charmander.me>
2018-10-26 19:18:56 -0700

    Improve performance by making better use of doubles

--- index.js
+++ index.js
@@ -1,100 +1,26 @@
 'use strict';
 
-// selected so (BASE - 1) * 0x100000000 + 0xffffffff is a safe integer
-var BASE = 1000000;
-
 function readInt8(buffer) {
 	var high = buffer.readInt32BE(0);
 	var low = buffer.readUInt32BE(4);
 	var sign = '';
 
-	if (high < 0) {
+	if (high < -0x200000) {
 		high = ~high + (low === 0);
 		low = (~low + 1) >>> 0;
 		sign = '-';
+	} else if (high < 0x200000) {
+		// 0x100000000 * 0x1fffff + 0xffffffff is a safe integer, and 0x100000000 * -0x200000 is the correct integer
+		return '' + (0x100000000 * high + low);
 	}
 
-	var result = '';
-	var carry;
-	var t;
-	var digits;
-	var pad;
-	var l;
-	var i;
+	// shift the last 4 decimal digits off, producing a safe integer in high:low (2**63 // 10000 is safe)
+	var trail = (7296 * high + low) % 10000;
+	var carry = high % 10000;
+	high = high / 10000 >>> 0;
+	low = (0x100000000 * carry + low) / 10000 >>> 0;
 
-	{
-		carry = high % BASE;
-		high = high / BASE >>> 0;
-
-		t = 0x100000000 * carry + low;
-		low = t / BASE >>> 0;
-		digits = '' + (t - BASE * low);
-
-		if (low === 0 && high === 0) {
-			return sign + digits + result;
-		}
-
-		pad = '';
-		l = 6 - digits.length;
-
-		for (i = 0; i < l; i++) {
-			pad += '0';
-		}
-
-		result = pad + digits + result;
-	}
-
-	{
-		carry = high % BASE;
-		high = high / BASE >>> 0;
-
-		t = 0x100000000 * carry + low;
-		low = t / BASE >>> 0;
-		digits = '' + (t - BASE * low);
-
-		if (low === 0 && high === 0) {
-			return sign + digits + result;
-		}
-
-		pad = '';
-		l = 6 - digits.length;
-
-		for (i = 0; i < l; i++) {
-			pad += '0';
-		}
-
-		result = pad + digits + result;
-	}
-
-	{
-		carry = high % BASE;
-		high = high / BASE >>> 0;
-
-		t = 0x100000000 * carry + low;
-		low = t / BASE >>> 0;
-		digits = '' + (t - BASE * low);
-
-		if (low === 0 && high === 0) {
-			return sign + digits + result;
-		}
-
-		pad = '';
-		l = 6 - digits.length;
-
-		for (i = 0; i < l; i++) {
-			pad += '0';
-		}
-
-		result = pad + digits + result;
-	}
-
-	{
-		carry = high % BASE;
-		t = 0x100000000 * carry + low;
-		digits = '' + t % BASE;
-
-		return sign + digits + result;
-	}
+	return sign + (0x100000000 * high + low) + ('' + (10000 + trail)).slice(1);
 }
 
 module.exports = readInt8;
